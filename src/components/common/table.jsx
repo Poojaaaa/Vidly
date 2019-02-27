58c6bbe1e879e3cd075bad35f3eee1c825939c5b
Poojaaaa <Pooja.gajera11@gmail.com>
2019-02-27 12:30:47 -0800

    Sorting - Extracting a Method

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -6,7 +6,7 @@ const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
-      <TableBody data={data} columns={this.columns} />
+      <TableBody data={data} columns={columns} />
     </table>
   );
 };
